95dc181fa49af68127df39c39582639803d85d49
dkirkwoo <dkirkwoo@cisco.com>
2017-11-08 16:34:15 +1100

    improve ngrok + webhook operation

--- createWebhook/createWebhook.py
+++ createWebhook/createWebhook.py
@@ -5,9 +5,10 @@ import json
 
 
 
-webhookID = settings.webhookID
-webhookName = settings.webhookName
+#webhookID = settings.webhookID
+webhookName = 'IndustrialOnboarding'
 botToken = settings.botToken
+botID = settings.botID
 
 
 
@@ -34,6 +35,57 @@ def findNgrok():
         time.sleep(1)
 
 
+# set spark headers
+def setHeaders(access_token):
+    accessToken_hdr = 'Bearer ' + access_token
+    spark_header = {
+        'Authorization':accessToken_hdr,
+        'Content-Type':'application/json; charset=utf-8',
+        }
+    return (spark_header)
+
+
+def findWebhooks(headers):
+    url = "https://api.ciscospark.com/v1/webhooks/"
+    response = requests.request("GET", url, headers=headers)
+    webhooks = json.loads(response.text)
+    return webhooks
+
+def createWebhook(headers, targetUrl):
+    url = "https://api.ciscospark.com/v1/webhooks/"
+    payloadRaw = {
+    "name": "IndustrialOnboarding",
+    "targetUrl": targetUrl,
+    "resource": "messages",
+    "event": "created",
+    "filter": "mentionedPeople=" + botID
+    }
+    payload = json.dumps(payloadRaw)
+
+    try:
+        response = requests.request("POST", url, data=payload, headers=headers, verify=False)
+        status_code = response.status_code
+        if (status_code == 200):
+            return True
+        else:
+            response.raise_for_status()
+            print("Error occured in POST -->"+(response.text))
+    except requests.exceptions.HTTPError as err:
+        print ("Error in connection -->"+str(err))
+
+
+
+
+# update webhook with updated ngrok tunnel information
+def updateWebhook (headers,webhookName,webhookID,targetUrl):
+    url = "https://api.ciscospark.com/v1/webhooks/" + webhookID
+    payload = "{\n\t\"name\": \"" + webhookName + "\",\n\t\"targetUrl\": \"" + targetUrl + "\"\n}"
+    response = requests.request("PUT", url, data=payload, headers=headers)
+    return response.status_code
+
+
+
+
 
 #Address of ngrok tunnel for Dev. Used as destination for Spark webhook notifications
 ngrokRaw = findNgrok()
@@ -49,29 +101,33 @@ for tun in ngrokRaw['tunnels']:
         print ('Ngrok tunnel not created')
 
 
-# set spark headers
-def set_headers(access_token):
-    accessToken_hdr = 'Bearer ' + access_token
-    spark_header = {
-        'Authorization':accessToken_hdr,
-        'Content-Type':'application/json; charset=utf-8',
-        }
-    return (spark_header)
+headers = setHeaders(botToken)
 
-# update webhook with updated ngrok tunnel information
-def update_webhook (the_headers,webhookName,webhookID,targetUrl):
-    url = "https://api.ciscospark.com/v1/webhooks/" + webhookID
-    payload = "{\n\t\"name\": \"" + webhookName + "\",\n\t\"targetUrl\": \"" + targetUrl + "\"\n}"
-    response = requests.request("PUT", url, data=payload, headers=the_headers)
-    return response.status_code
+webhookTest = False
 
+existingWebhook = findWebhooks(headers)
+print(existingWebhook)
 
-headers = set_headers(botToken)
-updateResult = update_webhook(headers, webhookName, webhookID, ngrokTunnel)
+if not existingWebhook['items']:
+    createTest = createWebhook(headers, ngrokTunnel)
+    if createTest == True:
+        webhookTest = True
+        print ('Webhook Created')
 
-if updateResult == 200:
-    print ('WebHook Updated')
 else:
-    print (updateResult)
+    for hooks in existingWebhook['items']:
+        if hooks['name'] == 'IndustrialOnboarding':        
+            updateResult = updateWebhook(headers, webhookName, hooks['id'], ngrokTunnel)
+
+            if updateResult == 200:
+                print ('WebHook Updated')
+                webhookTest = True
+            else:
+                print (updateResult)
+
+if webhookTest == False:
+    createHook = createWebhook(headers, ngrokTunnel)
+    if createHook == True:
+        print ('Webhook Created')
 
 #b
