b8478ab88aaf62ea8ad4b6216c1eb001636a73ae
dkirkwoo <dkirkwoo@cisco.com>
2017-11-08 17:35:28 +1100

    add settings template file

---
+++ settingsTemplate.py
@@ -0,0 +1,17 @@
+# This is a template for the settings.py file. The settings file is used to import unique and sensitive data into your application. 
+# When you fill in all the information in this template, save as settings.py in /createWebhook and /python
+
+# The IP Address of your ISE server. Eg server = '10.22.33.44'
+server = ''
+
+# The username of the ISE user who has ERSAdmin rights. Eg username = 'MyISEAPIAdmin' 
+username = ''
+
+# The password of the ISE user above. Eg password = '5trongP4ssw0rd12345'
+password = ''
+
+# The authentication token of your Spark bot. Is shown to you when you create the bot. Eg botToken = 'agfhyuuweERAdvxcvDFbv25gbfb000vxzcvSSS'
+botToken = ''
+
+# The unique identifier of your bot as a Spark user. Note this looks similar to your token but is different! Eg botID = 'LKGS556bvmnbsfddghlkhfdSRGGFGBfn'
+botID = ''
